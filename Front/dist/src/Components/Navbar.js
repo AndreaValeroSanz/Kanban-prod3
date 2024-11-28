5d6e6e86fc6e87c97ab6b50c4a96aaad188c9108
AndreaValeroSanz <avalerosanz@uoc.edu>
2024-11-28 11:34:33 +0100

    pesgato sin searchbar

--- Front/dist/src/Components/Navbar.js
+++ Front/dist/src/Components/Navbar.js
@@ -12,17 +12,6 @@ class Navbar extends HTMLElement {
 
     this.innerHTML = `
       <nav class="navbar p-0 m-0">
-        <div class="w-100 d-flex justify-content-between">
-          <div class="container">
-            <div class="bg-gray rounded-3 w-25 px-2">
-              <i class="bi bi-search bg-transparent"></i>
-              <input
-                type="text"
-                class="border-0 bg-transparent px-2"
-                placeholder="Search for anything..."
-              />
-            </div>
-          </div>
 
           ${
             userEmail
